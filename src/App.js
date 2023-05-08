b6b597d8f1e3299d1eb306b15a244043c6336461
APichPro <apichereau.pro@gmail.com>
2023-05-08 17:18:26 +0200

    Features/ChannelDetailPage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
         <NavBar />
         <Routes>
           <Route path='/' exact element={<Feed />} />
-          <Route path='/channels/:id' exact element={<ChannelDetail />} />
+          <Route path='/channel/:id' exact element={<ChannelDetail />} />
           <Route path='/video/:id' exact element={<VideoDetail />} />
           <Route path='/search/:searchTerm' exact element={<SearchFeed />} />
         </Routes>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { SideBar, Videos } from '../components';
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+    .then((data) => {setVideos(data.items)})
+  }, [searchTerm])
+  
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto' }}>
+      <Typography variant='h4' fontWeight="bold" md={2} sx={{color: 'white'}}>
+      Search result for <span style={{color: '#FC1503'}}>
+          {searchTerm}
+        </span> videos
+      </Typography >
+    <Videos videos={videos}/>
+  </Box>
   )
 }
 
